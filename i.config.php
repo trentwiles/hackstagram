e5072c08223b0115fe321b3399f5e4b20fb0681e
Riverside Rocks <trent@riverside.rocks>
2020-12-03 19:01:14 -0500

    Update

---
+++ i.config.php
@@ -0,0 +1,20 @@
+<?php
+/*==================================================/
+
+
+IMPORTANT! You will need to set up a few things here.
+
+Proxy Url: Proxies the Instagram API
+
+Image Proxy Url: Proxies Instagram Images
+
+Please use HTTPS!
+
+===================================================*/
+
+
+// Default is https://api-proxy.riverside.rocks, you can use this if you would like
+define("proxy", "https://api-proxy.riverside.rocks");
+
+// Default is https://emerald.riverside.rocks
+define("image_proxy", "https://emerald.riverside.rocks");
--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require '/i.config.php';
 
 $router = new \Bramus\Router\Router();
+$pug = new Pug();
 
+//(/[a-z0-9_-]+)
 
+$router->get('/', function() {
+    Phug::displayFile('views/index.pug');
+});
 
 $router->run();
